c88e76f743cd73c22985edfed22a19ea556ced7a
Benedikt Flöser <benedikt.floeser@scandio.de>
2024-06-10 13:39:23 +0200

    Remove proton pass again

--- config/scripts/proton.sh
+++ config/scripts/proton.sh
@@ -5,6 +5,3 @@ RELEASE=$(curl -s https://api.github.com/repos/ProtonMail/inbox-desktop/releases
 cd /tmp
 curl -LO https://github.com/ProtonMail/inbox-desktop/releases/download/"${RELEASE}"/proton-mail-"${RELEASE:1}"-1.x86_64.rpm
 rpm-ostree install ./proton-mail-"${RELEASE:1}"-1.x86_64.rpm
-
-curl -L https://proton.me/download/PassDesktop/linux/x64/ProtonPass.rpm --output pass.rpm
-rpm-ostree install ./pass.rpm
